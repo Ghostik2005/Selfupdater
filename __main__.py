f1bac3990475aa31404dfe5f59cc88e9f8be9e58
Ghostik2005 <sergobel78@gmail.com>
2017-07-27 13:42:29 +0300

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+import time
+
+def f_main():
+    c=0
+    while True:
+        c += 1
+        time.sleep(1.5)
+        print('main loop-->', c, end='\r', flush=True)
+
+
+
+if __name__ == '__main__':
+    """
+    first argument - main bussines function,
+    update_path - list of pathes for updates (local path (Linux-compatible or urls),
+    interval - timeout for next update check
+"""
+    import libs._reloader as rl
+    rl.run_reloader(
+        f_main,
+        update_path=[
+                '/ms71/temp/upd.zip',
+                'http://update.url',
+            ],
+        interval=5
+    )
